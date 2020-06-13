a1f3467e9678497e238da9aa52ee302df4b24ccc
Administrator <endsofjava@163.com>
2020-06-13 15:39:17 +0800

    新建项目

---
+++ src/main/java/com/zy/multithreading/MultithreadingApplication.java
@@ -0,0 +1,13 @@
+package com.zy.multithreading;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultithreadingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MultithreadingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zy/multithreading/MultithreadingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zy.multithreading;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultithreadingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
